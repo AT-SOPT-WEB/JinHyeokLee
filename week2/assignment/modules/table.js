bcd8e83e67f4c225091a89f0868b3dd2791d0698
constantly-dev <rntqhdl10@naver.com>
2025-04-25 16:18:18 +0900

    feat: add todo 기능 구현

--- week2/assignment/modules/table.js
+++ week2/assignment/modules/table.js
@@ -1,6 +1,6 @@
 import { todos } from '../data/todoData.js';
 
-const initStorage = () => {
+const initTable = () => {
   if (!localStorage.getItem('todos')) {
     localStorage.setItem('todos', JSON.stringify(todos));
   }
@@ -11,7 +11,11 @@ const getTodos = () => {
   return todos ? JSON.parse(todos) : [];
 };
 
-export const loadTable = () => {
+const setTodos = (updateTodos) => {
+  localStorage.setItem('todos', JSON.stringify(updateTodos));
+};
+
+export const renderTable = () => {
   const todos = getTodos();
   const tableBody = document.querySelector('tbody');
   tableBody.innerHTML = '';
@@ -19,16 +23,41 @@ export const loadTable = () => {
   todos.forEach((todo) => {
     const row = document.createElement('tr');
     row.innerHTML = `
-      <td>${todo.id}</td>
-      <td>${todo.title}</td>
-      <td>${todo.completed ? '✅' : '❌'}</td>
+      <td><input type="checkbox"/ data-id="${todo.id}"></td>
       <td>${todo.priority}</td>
+      <td>${todo.completed ? '✅' : '❌'}</td>
+      <td>${todo.title}</td>
     `;
     tableBody.appendChild(row);
   });
 };
 
-export const init = () => {
-  initStorage();
-  loadTable();
+const addTodo = () => {
+  const input = document.querySelector('input[type="text"]');
+  const priority = document.querySelector('#add_select');
+  const storedTodos = getTodos();
+
+  if (input.value.trim() === '' || !priority.value) {
+    alert('할 일과 중요도 중에 빈 값이 있습니다!');
+    return;
+  }
+
+  const newTodo = {
+    id: Date.now(),
+    title: input.value,
+    completed: false,
+    priority: priority.value,
+  };
+
+  const updatedTodos = [...storedTodos, newTodo];
+  setTodos(updatedTodos);
+  renderTable();
 };
+
+export const init = () => {
+  initTable();
+  renderTable();
+};
+
+const addButton = document.querySelector('#add_button');
+addButton.addEventListener('click', addTodo);
